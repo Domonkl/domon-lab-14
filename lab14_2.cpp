5861d08b84b0801f9e656c9d46fd677a9e8e81a3
Domonmez <domonsus@gmail.com>
2021-12-28 16:21:31 +0700

    ข้อ 2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -18,6 +18,37 @@ int main()
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+    }while(s != 0 ,  x != 0 , y != 0);
     return 0;
+}
+
+void updateImage(bool im[][M],int s,int x,int y){
+
+    for (int i = 0; i < N; i++)
+    {   for (int j= 0; j < M ; j++)
+        {
+            if (sqrt(pow(i-x,2)+pow(j-y,2))<=s-1)
+            {
+            im[i][j]=1;
+            }
+        }
+    }
+}
+
+
+
+
+void showImage(const bool sp[][M]){
+    cout<<"------------------------------------------------------------------------";
+    for (int i = 0; i < N; i++)
+    {
+        cout <<"\n"<< "|";
+        for (int j = 0; j < M; j++)
+    {
+       if(sp[i][j] == 1) cout <<"*"   ;
+        else cout <<" "   ;
+    }
+        cout << "|";
+    }
+    cout<<"\n------------------------------------------------------------------------\n";
 }
